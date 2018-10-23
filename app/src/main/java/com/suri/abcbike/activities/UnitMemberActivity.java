6923b8eaf9a012c2db680cecd72c72e50815151f
yoon sang woon <tkaltk123@gmail.com>
2018-10-23 19:12:41 +0900

    부대생성 추가

--- app/src/main/java/com/suri/abcbike/activities/UnitMemberActivity.java
+++ app/src/main/java/com/suri/abcbike/activities/UnitMemberActivity.java
@@ -1,8 +1,8 @@
 package com.suri.abcbike.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -18,12 +18,12 @@ public class UnitMemberActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        FloatingActionButton makeUnitBtn = (FloatingActionButton) findViewById(R.id.unit_make);
+        final Intent i=new Intent(this,makeUnitActivity.class);
+        makeUnitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/suri/abcbike/activities/makeUnitActivity.java
+++ app/src/main/java/com/suri/abcbike/activities/makeUnitActivity.java
@@ -1,13 +1,28 @@
 package com.suri.abcbike.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.suri.abcbike.R;
+
 
 public class makeUnitActivity extends AppCompatActivity {
 
+    Spinner spinner;
+    ArrayAdapter sAdapter;
+    String[] item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_unit);
+
+        item=getResources().getStringArray(R.array.unit_head);
+        spinner = (Spinner) findViewById(R.id.unit_category);
+        sAdapter = new ArrayAdapter(this,R.layout.support_simple_spinner_dropdown_item,item);
+        spinner.setAdapter(sAdapter);
+
+
     }
 }
