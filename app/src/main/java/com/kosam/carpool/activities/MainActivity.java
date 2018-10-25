f5b4b512bf5b84eadc1640cc0449542fe847015d
yoon sang woon <tkaltk123@gmail.com>
2018-10-25 10:09:10 +0900

    update main

--- app/src/main/java/com/kosam/carpool/activities/MainActivity.java
+++ app/src/main/java/com/kosam/carpool/activities/MainActivity.java
@@ -25,14 +25,19 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected static final String TAG = MainActivity.class.getSimpleName();
 
+    public static MainActivity thisActivity;
     private SharedPreferences mPreferences;
-
-    private NavigationView navigationView;
     private View mLayout;
-
-
+    //navigation view 연결
+    private NavigationView navigationView;
     private TextView NavUserName;
-
+    private TextView NavUserTopUnit;
+    private TextView NavUserUnitName;
+    private TextView NavUserEmail;
+    //ui참조
+    ListviewAdapter adapter;
+    ListView listview;
+    FloatingActionButton carpoolMakeFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,28 +45,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //컨텍스트 참조
+        thisActivity=this;
 
-        //initCollapsingToolbar();
+        //ui연결
+        listview=(ListView)findViewById(R.id.carpool_list);
+        carpoolMakeFab = (FloatingActionButton) findViewById(R.id.carpool_make_fab);
 
-        ListviewAdapter adapter=new ListviewAdapter();
-        ListView listview=(ListView)findViewById(R.id.carpool_list);
+        //리스트뷰에 어댑터 연결
+        adapter=new ListviewAdapter();
         listview.setAdapter(adapter);
-        adapter.addItem(new ListviewItem(new Date(123123),"부대","간부 아파트","정태훈",1,4));
-        adapter.addItem(new ListviewItem(new Date(123123),"간부아파트","부대","윤상운",1,4));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.carpool_add_fab);
-        final Intent carpoolIntent=new Intent(this,CarpoolMakeActivity.class);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //fab에 카풀 생성 액티비티 연결
+        carpoolMakeFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(carpoolIntent);
+                Intent gotoMakeCarpool=new Intent(thisActivity,CarpoolMakeActivity.class);
+                startActivity(gotoMakeCarpool);
             }
         });
 
-        Intent i = getIntent();
-        if (!i.getBooleanExtra("auto",false)){
-            LoginActivity mPreActivity = (LoginActivity) LoginActivity.mThisActivity;
-            mPreActivity.finish();
+        Intent thisIntent = getIntent();
+        if (!thisIntent.getBooleanExtra("auto",false)){
+           LoginActivity.mThisActivity.finish();
         }
 
 
@@ -82,17 +88,18 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.bringToFront();
 
-        View header = navigationView.getHeaderView(0);
-
-        NavUserName = (TextView) header.findViewById(R.id.nav_head_name);
-
-        NavUserName.setText(mPreferences.getString("Name","Save The Color"));
-
-
+        NavUserName = (TextView) findViewById(R.id.nav_head_name);
+        NavUserTopUnit = (TextView) findViewById(R.id.nav_head_topunit);
+        NavUserUnitName = (TextView) findViewById(R.id.nav_head_unitname);
+        NavUserEmail = (TextView) findViewById(R.id.nav_head_email);
+        NavUserName.setText(mPreferences.getString("Name","ROCA"));
+        NavUserTopUnit.setText(mPreferences.getString("TopUnit","육군"));
+        NavUserUnitName.setText(mPreferences.getString("UnitName","국방부"));
+        NavUserEmail.setText(mPreferences.getString("Email",""));
     }
 
 
