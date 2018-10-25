8f00b0b3fe22e9265de63174b90096366b3aa5ab
air_defense_jung <wxg1297@gmail.com>
2018-10-25 10:56:39 +0900

    main activity temp input server connect

--- app/src/main/java/com/kosam/carpool/activities/MainActivity.java
+++ app/src/main/java/com/kosam/carpool/activities/MainActivity.java
@@ -1,8 +1,13 @@
 package com.kosam.carpool.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -11,19 +16,40 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.kosam.carpool.R;
+import com.savagelook.android.UrlJsonAsyncTask;
 
+import org.apache.http.client.HttpResponseException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Date;
 
 // , OnMapReadyCallback
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     protected static final String TAG = MainActivity.class.getSimpleName();
+
+    private MainActivity.CarpoolTask mCarpoolTask = null;
+    private String mURL = "https://kosam-app-server.run.goorm.io/api/carpools";
+
+    private View mProgressView;
+    private View mCarpoolListView;
 
     public static MainActivity thisActivity;
     private SharedPreferences mPreferences;
@@ -52,6 +78,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         //ui연결
         listview=(ListView)findViewById(R.id.carpool_list);
         carpoolMakeFab = (FloatingActionButton) findViewById(R.id.carpool_make_fab);
+
+        mCarpoolListView = findViewById(R.id.carpool_list_view);
+        mProgressView = findViewById(R.id.unit_progress);
 
         //리스트뷰에 어댑터 연결
         adapter=new ListviewAdapter();
@@ -193,4 +222,138 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         return super.onOptionsItemSelected(item);
     }
 */
+
+    private class CarpoolTask extends UrlJsonAsyncTask {
+
+
+        public CarpoolTask(Context context) {
+            super(context);
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+
+            DefaultHttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(urls[0]);
+            JSONObject holder = new JSONObject();
+            JSONObject carpoolObj = new JSONObject();
+            String response = null;
+            JSONObject json = new JSONObject();
+
+            try {
+                try {
+                    // setup the returned values in case
+                    // something goes wrong
+                    json.put("success", false);
+                    json.put("info", "Something went wrong.");
+                    // add the user email and password to
+                    // the params
+                    carpoolObj.put("auth_token", mPreferences.getString("AuthToken",""));
+                    holder.put("carpool", carpoolObj);
+                    StringEntity se = new StringEntity(holder.toString());
+                    post.setEntity(se);
+
+                    // setup the request headers
+                    post.setHeader("Accept", "application/json");
+                    post.setHeader("Content-Type", "application/json");
+
+                    ResponseHandler<String> responseHandler = new BasicResponseHandler();
+                    response = client.execute(post, responseHandler);
+                    json = new JSONObject(response);
+
+                } catch (HttpResponseException e) {
+                    e.printStackTrace();
+                    Log.e("ClientProtocol", "" + e);
+                    json.put("info", "Email and/or password are invalid.");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e("IO", "" + e);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Log.e("JSON", "" + e);
+            }
+
+            return json;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if (json.getBoolean("success")) {
+                    // everything is ok
+
+                    //SharedPreferences.Editor editor = mPreferences.edit();
+                    // save the returned auth_token into
+                    // the SharedPreferences
+                    JSONArray jCarpoolArray = json.getJSONObject("data").getJSONArray("carpool");
+                    if(jCarpoolArray != null) {
+
+                        for(int i = 0; i <jCarpoolArray.length(); i++) {
+                            JSONObject jobj = jCarpoolArray.getJSONObject(i);
+                            //Pair j_pair = new Pair(jobj.getInt("id"), jobj.getString("unit_name"));
+                            //mCarpoolList.add(j_pair);
+                        }
+
+                        //여기서부터 mUnitList를 이용하여 List에 자료 넣기 코드 넣기
+
+                    }
+
+
+                } else {
+                    showProgress(false);
+                }
+                Toast.makeText(context, json.getString("info"), Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                // something went wrong: show a Toast
+                // with the exception message
+                Toast.makeText(context, e.getMessage(), Toast.LENGTH_LONG).show();
+                showProgress(false);
+            } finally {
+                super.onPostExecute(json);
+            }
+        }
+
+        @Override
+        protected void onCancelled() {
+            mCarpoolTask = null;
+            showProgress(false);
+        }
+
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mCarpoolListView.setVisibility(show ? View.GONE : View.VISIBLE);
+            mCarpoolListView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mCarpoolListView.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mCarpoolListView.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
+    }
 }
